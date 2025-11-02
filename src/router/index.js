b42b44ba10517b94ac7cdfd2a4170dd26d300ef7
Astrr1127 <liuzy269@mail2.sysu.edu.cn>
2025-11-02 23:49:03 +0800

    登录逻辑跳转

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import login from '@/pages/login.vue'
 import register from '@/pages/register.vue'
 import forgotPassword from "@/pages/forgot-password.vue";
 import welcomehome from '@/pages/welcomehome.vue'
+import Profile from '@/components/profile.vue'
 const routes = [
   {
     path: '/',
@@ -41,6 +42,11 @@ const routes = [
     path: '/forgot-password',
     component: forgotPassword,
   },
+  {
+      path: '/profile', 
+      name: 'Profile',
+      component: Profile 
+    }
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
